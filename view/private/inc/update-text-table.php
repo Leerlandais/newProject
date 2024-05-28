604f3f1bcb28df95f38e909a41f7ac3ed1b2276a
leerlandais <leerlandais@gmail.com>
2024-05-28 22:07:44 +0200

    time to dream up a solution

--- view/private/inc/update-text-table.php
+++ view/private/inc/update-text-table.php
@@ -4,9 +4,9 @@
                         <table class="table table-bordered table-striped" data-toggle="table" data-show-columns="true" data-search="true"data-pagination="true">
                         <thead>
                                 <tr>
-                                    <th class="text-center" id="updateTableHeadId"></th>
-                                    <th class="text-center" id="updateTableHeadName"></th>
-                                    <th class="text-center" id="updateTableHeadText"></th>
+                                    <th class="text-center updateTableHeadId"></th>
+                                    <th class="text-center updateTableHeadName"></th>
+                                    <th class="text-center updateTableHeadText"></th>
                                     <th class="text-center">Modifier</th>
   
                                     
@@ -14,8 +14,7 @@
                             </thead>
                             <tbody>
                                 <?php
-                                    
-                                        
+                                    // replace these with a foreach - $allText                                        
                                         ?>
                                     <tr>
                                         <td><? ?></td>
